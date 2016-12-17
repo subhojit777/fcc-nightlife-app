b452bd3e6da180326316c197e36ae4155423ca80
Subhojit Paul <subhojitpaul21@gmail.com>
2016-12-18 00:44:30 +0530

    semantic ui

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'semantic/dist')));
+app.use(express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 
 // Session will expire after 1 week.
 app.use(session({
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,10 @@ var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Nighlife App',
+    loggedIn: req.user ? true : false
+  });
 });
 
 router.get('/auth/twitter', passport.authenticate('twitter'));
